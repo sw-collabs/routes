106f79430611b36b64abd2f3c2f94bbbb5569e47
Jimmy Li <dfjimmy.li@gmail.com>
2020-05-19 15:16:38 -0400

    minor changes

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import {__ns, circle, vec} from "./lib.js";
 const ID_SVG = 'main-svg';
 
 (() => {
-  alert('hi there my name is jimmy');
   let SVG = document.getElementById(ID_SVG);
   __ns(SVG, {},
     circle(vec(100,100), 30, {
